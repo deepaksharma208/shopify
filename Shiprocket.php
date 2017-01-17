fc563bc6fb4ce24be3ce6256c87d33e48bb14bb2
dpk208 <deepak.sharma@kartrocket.com>
2017-01-17 18:13:58 +0530

    Adding shiprocket.php

---
+++ Shiprocket.php
@@ -0,0 +1,5 @@
+<?php
+// PHP permanent URL redirection
+header("Location: https://app.shiprocket.in/register?utm_source=Shopify-AppStore&utm_medium=Referal&utm_campaign=Shopify-App-Download", true, 301);
+exit();
+?>
